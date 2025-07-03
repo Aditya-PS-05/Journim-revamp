9c6bf345162e90037653a29d70361eb3803522a4
Aditya-PS-05 <adityapratapsjnhh7654@gmail.com>
2025-07-03 21:32:28 +0530

    fix: locateIndia

--- components/base/LocateIndia/LocateIndia.tsx
+++ components/base/LocateIndia/LocateIndia.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const LocateIndia = () => {
   return (
-    <section className="relative w-full min-h-screen bg-[#67D8E6] flex items-center justify-center py-12">
+    <section className="relative w-full max-h-screen bg-[#67D8E6] flex items-center justify-center py-16 px-4">
       <div className="w-full flex justify-center items-center">
         <img 
           src="/images/locations/india.png" 
